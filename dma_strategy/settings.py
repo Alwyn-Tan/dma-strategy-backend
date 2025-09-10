97dcbf07e1747428c11c75c825682514b2a303e6
Alwyn-Tan <2660412180@qq.com>
2025-09-10 22:35:38 +0800

    fix: settings application name

--- dma_strategy/settings.py
+++ dma_strategy/settings.py
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'stock_project.urls'
+ROOT_URLCONF = 'dma_strategy.urls'
 
 TEMPLATES = [
     {
@@ -56,7 +56,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'stock_project.wsgi.application'
+WSGI_APPLICATION = 'dma_strategy.wsgi.application'
 
 DATABASES = {
     'default': {
